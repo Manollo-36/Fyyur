48b654d4e981afdd05689681ffeb9cce75ec630f
Emmanuel Varsakis <varsakise@gmail.com>
2024-10-19 17:05:40 +0300

    Implementation for adding album and Not allowing shows to be booked based on date

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_wtf import Form
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL
+from wtforms import FieldList, StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
+from wtforms.validators import DataRequired, AnyOf, URL,Optional
 
 class ShowForm(Form):
     artist_id = StringField(
@@ -114,7 +114,7 @@ class VenueForm(Form):
         ]
     )
     facebook_link = StringField(
-        'facebook_link', validators=[URL()]
+        'facebook_link', validators=[URL(),Optional()]
     )
     website_link = StringField(
         'website_link'
@@ -193,7 +193,7 @@ class ArtistForm(Form):
     )
     phone = StringField(
         # TODO implement validation logic for phone 
-        'phone'
+        'phone' 
     )
     image_link = StringField(
         'image_link'
@@ -224,7 +224,7 @@ class ArtistForm(Form):
      )
     facebook_link = StringField(
         # TODO implement enum restriction
-        'facebook_link', validators=[URL()]
+        'facebook_link', validators=[URL(),Optional()]
      )
 
     website_link = StringField(
@@ -236,4 +236,21 @@ class ArtistForm(Form):
     seeking_description = StringField(
             'seeking_description'
      )
+    
+class AlbumForm(Form):
+    artist_id = StringField(
+        'artist_id'
+    )
+
+    album_name = StringField(
+        'album_name'
+    )
+
+    album_cover_link = StringField(
+        'album_cover_link'
+    )
+
+    song_names = StringField('Songs')
+    
+
 
